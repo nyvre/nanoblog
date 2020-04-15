e7fb7b20f7fc3618c1e72e9784c7b66c9ad66bd1
nyvre <nyvre@users.noreply.github.com>
2020-04-15 10:29:14 +0200

    change js files location

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ var app = express();
 
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
-
+app.use(express.static(path.join(__dirname, '/public/assets/js')))
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);

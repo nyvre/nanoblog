c3a1c738ff9fd31442c5f96f3e3690d93956e3b9
nyvre <nyvre@users.noreply.github.com>
2020-04-15 12:38:23 +0200

    created working login/signup/logout user system

--- public/assets/js/user_actions.js
+++ public/assets/js/user_actions.js
@@ -1,27 +1,46 @@
 function createUserInDatabase(username, password) {
+	Parse.initialize("nanoblogo");
+	Parse.serverURL = "http://localhost:8080/parse";
 	var user = new Parse.User();
 	user.set("username", username);
 	user.set("password", password);
 	user.signUp()
 		.then(function(success) {
-			console.log("User has been created")
+			window.open("http://localhost:8080/test", "_self");
 	}, function(err) {
 		console.error(err);
 	});
 }
 
-function logInUser(username, password) {
+function logInUser (username, password) {
+	Parse.initialize("nanoblogo");
+	Parse.serverURL = "http://localhost:8080/parse";
 	var user = new Parse.User();
 	user.set("username", username);
 	user.set("password", password);
 	user.logIn()
 		.then(function(success) {
-			console.log("User has been logged in")
+			window.open("http://localhost:8080/test", "_self");
 	}, function(err) {
 		console.error(err);
 	});
 }
 
-Parse.initialize("nanoblogo");
-Parse.serverURL = "http://localhost:8080/parse";
+function logOutCurrentUser() {
+	Parse.initialize("nanoblogo");
+	Parse.serverURL = "http://localhost:8080/parse";
+	Parse.User.logOut()
+		.then(function (success) {
+			console.log("User has been logged out");
+	}, function(err) {
+	console.error(err);
+	});
+
+}
+function getCurrentUser() {
+	Parse.initialize("nanoblogo");
+	Parse.serverURL = "http://localhost:8080/parse";
+	return Parse.User.current().getUsername();
+
+}
 

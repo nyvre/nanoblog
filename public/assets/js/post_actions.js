d00527186980e1316c6f071f1c52c3bfd47f1778
nyvre <nyvre@users.noreply.github.com>
2020-04-16 11:00:51 +0200

    new posts are updating without page refreshing

--- public/assets/js/post_actions.js
+++ public/assets/js/post_actions.js
@@ -1,26 +1,39 @@
-function getPosts() {
-	Parse.initialize("nanoblogo");
-	Parse.serverURL = "https://nanoblogo.herokuapp.com/parse";
-	var allPosts = new Parse.Query("posts");
-	allPosts.ascending("createdAt");
-	allPosts.include("author");
-	return allPosts.find().then(function(posts) {
+var getPosts = function() {
+	return new Promise(function (resolve, reject) {
+		Parse.initialize("nanoblogo");
+		Parse.serverURL = "https://nanoblogo.herokuapp.com/parse";
+		var allPosts = new Parse.Query("posts");
+		allPosts.ascending("createdAt");
+		allPosts.include("author");
+		return resolve(allPosts.find());
+})};
+var addPostToDatabase = function(body) {
+	return new Promise(function(resolve, reject) {
+		Parse.initialize("nanoblogo");
+		Parse.serverURL = "https://nanoblogo.herokuapp.com/parse";
+		var Post = Parse.Object.extend("posts");
+		post = new Post();
+		post.set("body", body);
+		post.set("author", Parse.User.current());
+		return(resolve(post.save()));
+	});
+}
+var formatPosts = function (posts) {
+	return new Promise(function(resolve, reject) {
 		let postsFormated = [];
 		for (post of posts) {
 			postsFormated.push([post.get("author").get("username"), post.get("body"), post.get("createdAt")]);
 		}
-		return postsFormated;
+		return resolve(postsFormated);
 	});
 }
-function addPostToDatabase(body) {
-	Parse.initialize("nanoblogo");
-	Parse.serverURL = "https://nanoblogo.herokuapp.com/parse";
-	var Post = Parse.Object.extend("posts");
-	post = new Post();
-	post.set("body", body);
-	post.set("author", Parse.User.current());
-	post.save().then(function (result) {
-		window.open("/", "_self");
-		});
+function getAndFormatPosts() {
+	return new Promise(function(resolve, reject) {
+		return resolve(getPosts()
+			.then(function (posts) {
+				return formatPosts(posts)
+			})
+		)
+	});
 }
 

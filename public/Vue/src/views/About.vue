84569224f93a3f10abb8f661783d3b06886662fe
Dominik Kukiełka <dominik.kukielka97@gmail.com>
2020-04-29 20:56:56 +0200

    added about page

--- public/Vue/src/views/About.vue
+++ public/Vue/src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
-    <h1>This is an about page. Page about nanoblogo ziom</h1>
+    <h1 align=center>Witaj na nanoblogu</h1>
+    <div class = "block">
+    <p>Nanoblog to platforma, która łączy ludzi. Została ona stworzona w celu wymiany myśli między członkami naszej społeczności.</p>
+    <ul>W skład naszego zespołu wchodzą:
+    <li><b>Marcin</b> - Lider naszego zespołu i główny mózg operacji. Człowiek wielu talentów. To jemu zawdzięczamy większość back-endu</li>
+    <li><b>Dominik K</b> - starał się żeby interfejs użytkownika był jak najbardziej przyjemny dla oka oraz usprawniał działanie strony</li>
+    <li><b>Dominik G</b> - odpowiada za spięcie części i ostateczne porządki</li></ul>
+    </div>
   </div>
 </template>
+<style scoped>
+.about {
+  margin:5px; padding:5px;
+  color:#006666;
+  margin: 25px 50px 75px;
+  line-height: 2.7;
+  text-align: center;
+}
+h1 {
+  font-size:300%;
+  line-height: 1.5;
+}
+</style>

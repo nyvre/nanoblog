02c7323aae2435f13df2e4bfae0f89eb5b119198
nyvre <nyvre@users.noreply.github.com>
2020-04-29 08:48:29 +0200

    FIX: adding comments do not refresh whole site

--- public/Vue/src/components/Posts.vue
+++ public/Vue/src/components/Posts.vue
@@ -4,11 +4,11 @@
       <a v-if="username!=''" id="currentUser"><li>Jesteś zalogowany jako {{username}}</li></a>
       <textarea type="text" id="post-body" name="post-body" class='textbox' wrap="soft" />
       <input type="submit" v-on:click="addPost" class="add-post-btn" style="color:green;background:#ffffe6;border-radius:10px" > 
-    </div>
+    </div>,
     <div v-bind:key='post.objectId' v-for='post in posts' :id='post.objectId' name='post' class='chat'>
       <p class='chatinfo'> <span class='author'>{{post.author}}</span> {{post.createdAt}} Points: {{post.points}} <input type="submit" v-on:click="addPoint(post.objectId)" class="add-point-btn" style="color:green;background:#ffffe6;border-radius:10px" value="+"></p>
       <p class='messagebox'>{{post.body}}</p>
-      <p> {{post.comments}} </p>
+      <p> {{comments[post.objectId]}} </p>
       <textarea type="text" :id="post.objectId + '-comment'" name="comment-body" class='textbox' wrap="soft" />
       <input type="submit" v-on:click="addComment(post.objectId)" class="add-comment-btn" style="color:green;background:#ffffe6;border-radius:10px" >
       <br>
@@ -25,7 +25,8 @@
     name: "Posts",
     data: function() {
       return {
-        posts: []
+        posts: [],
+        comments: []
       }
     },
     props: ["username"],
@@ -41,20 +42,14 @@
         });
       },
       formatPosts (posts) {
-        var self = this;
         return new Promise(function(resolve) {
           let postsFormated = [];
           for (let post of posts) {
-            Promise.all([self.getAndFormatComments(post.id), self.getPoints(post.id)])
-            .then(([comments, points]) => {
-              postsFormated.push({
-                "objectId": post.id, 
-                "author": post.get("author").get("username"), 
-                "body": post.get("body"), 
-                "comments": comments,
-                "points": points,
-                "createdAt": post.get("createdAt")
-              })
+            postsFormated.push({
+              "objectId": post.id, 
+              "author": post.get("author").get("username"), 
+              "body": post.get("body"), 
+              "createdAt": post.get("createdAt")
             })
           }
           return resolve(postsFormated);
@@ -70,36 +65,48 @@
           )
         });
       },
-      getComments (postId) {
+      getComments () {
         return new Promise(function (resolve) {
           Parse.initialize("nanoblogo");
           Parse.serverURL = "https://nanoblogo.herokuapp.com/parse";
           var Comment = new Parse.Query("Comment");
-          var Post = Parse.Object.extend('posts');
-          Comment.equalTo("parentPost", new Post({id : postId}));
-          Comment.ascending("createdAt");
+          Comment.ascending("createdAt");               
           Comment.include("author");
+          Comment.include("parentPost");
           return resolve(Comment.find());
         });
       },
       formatComments (comments) {
         return new Promise(function(resolve) {
-          let commentsFormated = [];
+          let commentsFormated = {};
           for (let comment of comments) {
-            commentsFormated.push({
-              "objectId": comment.id, 
-              "author": comment.get("author").get("username"), 
-              "body": comment.get("body"), 
-              "createdAt": comment.get("createdAt")
-            })
+            let key = comment.get("parentPost").id;
+            console.log(key);
+            if (commentsFormated[key]) {
+              console.log(commentsFormated[key]);
+              commentsFormated[key].push({
+                "objectId": comment.id, 
+                "author": comment.get("author").get("username"), 
+                "body": comment.get("body"), 
+                "createdAt": comment.get("createdAt")
+              })
+            } else {
+              commentsFormated[key] = [{
+                "objectId": comment.id, 
+                "author": comment.get("author").get("username"), 
+                "body": comment.get("body"), 
+                "createdAt": comment.get("createdAt")
+              }]
+            }
           }
+          console.log("koniec" + commentsFormated);
           return resolve(commentsFormated);
         });
       },
-      getAndFormatComments(postId) {
+      getAndFormatComments() {
         var self = this;
         return new Promise(function(resolve) {
-          return resolve(self.getComments(postId)
+          return resolve(self.getComments()
             .then(function (comments) {
               return self.formatComments(comments)
             })
@@ -123,7 +130,8 @@
         let comment = document.getElementById(parentPostId + "-comment");
         let commentBody = comment.value;
         comment.value = "";
-        this.addCommentToDatabase(commentBody, parentPostId).then(() => {this.populatePostsData()});
+        this.addCommentToDatabase(commentBody, parentPostId)
+          .then(() => {this.populateCommentsData()});
       },
       getPoints (postId) {
         return new Promise(function (resolve) {
@@ -154,6 +162,12 @@
             this.posts = retrieviedPosts.reverse();
           })
       },
+      populateCommentsData() {
+        this.getAndFormatComments()
+          .then((retrieviedComments) => {
+            this.comments = retrieviedComments;
+          })
+      },
       addPostToDatabase(body) {
         return new Promise(function(resolve) {
           Parse.initialize("nanoblogo");
@@ -169,7 +183,8 @@
         let post = document.getElementById("post-body");
         let postBody = post.value;
         post.value = "";
-        this.addPostToDatabase(postBody).then(() => {this.populatePostsData()});
+        this.addPostToDatabase(postBody)
+          .then(() => {this.populatePostsData()});
       },
       deletePost(objectId) {
         Parse.initialize("nanoblogo");
@@ -187,12 +202,13 @@
           });
       },
       checkForNewPosts () {
-        setInterval(this.populatePostsData, 10000);
+        setInterval(this.populateCommentsData, 10000);
       }
     },
     mounted () {
       this.populatePostsData();
-      //this.checkForNewPosts();
+      this.populateCommentsData();
+      this.checkForNewPosts();
     },
   }
 </script>
